530781378670ffb4f91a993fffa39d9ec63cc2cf
abhishek36 <akamble9913@gmail.com>
2022-01-21 12:23:39 +0530

    first commit

---
+++ src/controllers/room.controller.js
@@ -0,0 +1,30 @@
+//import Room model here
+
+const addRoom = async (req, res) => {
+  //addRoom api logic here
+};
+
+const getRoomByLocation = async (req, res) => {
+  //getRoomByLocation api logic here
+};
+
+const getRoomById = async (req, res) => {
+  //getRoomById api logic here
+};
+
+const updateRoomById = async (req, res) => {
+  //updateRoomById api logic here
+};
+
+const deleteRoomById = async (req, res) => {
+  //deleteRoomById api logic here
+};
+
+const RoomController = {
+  addRoom,
+  getRoomByLocation,
+  getRoomById,
+  updateRoomById,
+  deleteRoomById,
+};
+module.exports = RoomController;
